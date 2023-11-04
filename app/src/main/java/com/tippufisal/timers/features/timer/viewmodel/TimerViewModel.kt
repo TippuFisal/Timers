97c09f097b260e9ae4ae9c38b8fb15e9e74609e3
Tippu <fisal17.tf@gmail.com>
2023-11-04 18:32:31 +0530

    *Code clean up

--- app/src/main/java/com/tippufisal/timers/features/timer/viewmodel/TimerViewModel.kt
+++ app/src/main/java/com/tippufisal/timers/features/timer/viewmodel/TimerViewModel.kt
@@ -33,12 +33,6 @@ class TimerViewModel : ViewModel() {
         timerJob?.cancel()
     }
 
-    private fun formatTime(seconds: Long): String {
-        val minutes = seconds / 60
-        val remainingSeconds = seconds % 60
-        return String.format("%02d:%02d", minutes, remainingSeconds)
-    }
-
     override fun onCleared() {
         super.onCleared()
         timerJob?.cancel()
